afe10797180f18d6d856dcf09dec930e43cfa7de
Sai Krishna <YOUR_GITHUB_EMAIL>
2026-06-22 21:52:20 +0530

    Updated portfolio website

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
 console.log("Sai Krishna Portfolio Loaded Successfully!");
+const reveals = document.querySelectorAll(".reveal");
+
+function revealSections() {
+
+    reveals.forEach((section) => {
+
+        const windowHeight = window.innerHeight;
+
+        const revealTop = section.getBoundingClientRect().top;
+
+        const revealPoint = 120;
+
+        if (revealTop < windowHeight - revealPoint) {
+
+            section.classList.add("active");
+
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealSections);
+
+revealSections();
